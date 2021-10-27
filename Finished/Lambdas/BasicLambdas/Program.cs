1f8e8f8fe576df8176b182beddc8721061a69cf6
Joe Marini <cjmarini68@gmail.com>
2021-10-27 11:33:38 -0700

    remove foo and bar names

--- Finished/Lambdas/BasicLambdas/Program.cs
+++ Finished/Lambdas/BasicLambdas/Program.cs
@@ -12,24 +12,24 @@ namespace BasicLambdas
         static void Main(string[] args)
         {
             // Create a basic delegate that squares a number
-            MyDelegate foo = (x) => x * x;
-            Console.WriteLine("The result of foo is: {0}", foo(5));
+            MyDelegate d1 = (x) => x * x;
+            Console.WriteLine("The result of d1 is: {0}", d1(5));
 
             // Dynamically change the delegate to something else
-            foo = (x) => x * 10;
-            Console.WriteLine("The result of bar is: {0}", foo(5));
+            d1 = (x) => x * 10;
+            Console.WriteLine("The result of d1 is: {0}", d1(5));
 
             // Create a delegate that takes multiple arguments
-            MyDelegate2 bar = (x, y) =>
+            MyDelegate2 d2 = (x, y) =>
             {
                 Console.WriteLine("The two-arg lambda: {1}, {0}", x * 10, y);
             };
-            bar(25, "Some string");
+            d2(25, "Some string");
 
             // Define an expression delegate
-            ExprDelegate baz = (x) => x > 10;
-            Console.WriteLine("Calling baz with 5: {0}", baz(5));
-            Console.WriteLine("Calling bax with 15: {0}", baz(15));
+            ExprDelegate d3 = (x) => x > 10;
+            Console.WriteLine("Calling d3 with 5: {0}", d3(5));
+            Console.WriteLine("Calling d3 with 15: {0}", d3(15));
 
             // Keep the Command Line window open
             Console.WriteLine("\nPress Enter Key to Continue...");
