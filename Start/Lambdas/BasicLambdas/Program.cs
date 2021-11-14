00c62233029cc8d049af2ef7f31ac64b2678eda0
joemarini <cjmarini68@gmail.com>
2021-11-13 16:03:28 -0800

    Add lambdas to start state

---
+++ Start/Lambdas/BasicLambdas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BasicLambdas
+{
+    // define a few delegate types
+    public delegate int MyDelegate(int x);
+    public delegate void MyDelegate2(int x, string prefix);
+    public delegate bool ExprDelegate(int x);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a basic delegate that squares a number
+            MyDelegate d1 = (x) => x * x;
+            Console.WriteLine("The result of d1 is: {0}", d1(5));
+
+            // Dynamically change the delegate to something else
+            d1 = (x) => x * 10;
+            Console.WriteLine("The result of d1 is: {0}", d1(5));
+
+            // Create a delegate that takes multiple arguments
+            MyDelegate2 d2 = (x, y) =>
+            {
+                Console.WriteLine("The two-arg lambda: {1}, {0}", x * 10, y);
+            };
+            d2(25, "Some string");
+
+            // Define an expression delegate
+            ExprDelegate d3 = (x) => x > 10;
+            Console.WriteLine("Calling d3 with 5: {0}", d3(5));
+            Console.WriteLine("Calling d3 with 15: {0}", d3(15));
+
+            // Keep the Command Line window open
+            Console.WriteLine("\nPress Enter Key to Continue...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Start/Lambdas/LambdasSolution/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LambdasSolution
+{
+    public delegate void BalanceEventHandler(decimal theValue);
+
+    class PiggyBank
+    {
+        private decimal m_bankBalance;
+        public event BalanceEventHandler balanceChanged;
+
+        public decimal theBalance
+        {
+            set
+            {
+                m_bankBalance = value;
+                balanceChanged(value);
+            }
+            get
+            {
+                return m_bankBalance;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PiggyBank pb = new PiggyBank();
+
+            pb.balanceChanged += (amount) => Console.WriteLine("The balance amount is {0}", amount);
+            pb.balanceChanged += (amount) => { if (amount > 500.0m) Console.WriteLine("You reached your savings goal! You have {0}", amount); };
+
+            string theStr;
+            do
+            {
+                Console.WriteLine("How much to deposit?");
+
+                theStr = Console.ReadLine();
+                if (!theStr.Equals("exit"))
+                {
+                    decimal newVal = decimal.Parse(theStr);
+
+                    pb.theBalance += newVal;
+                }
+            } while (!theStr.Equals("exit"));
+        }
+    }
+}
