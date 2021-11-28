4c08c5140c28fd70710ec87e31671de2391b8c01
Joe Marini <cjmarini68@gmail.com>
2021-11-27 20:14:43 -0800

    Update composable delegates examples

--- Start/Delegates/Composable2/Program.cs
+++ Start/Delegates/Composable2/Program.cs
@@ -3,17 +3,16 @@
 namespace Composable2
 {
     // declare the delegate type
-    public delegate void MyDelegate(int arg1, ref int arg2);
+    public delegate void MyDelegate(int arg1, int arg2);
 
     class Program
     {
-        static void func1(int arg1, ref int arg2)
+        static void func1(int arg1, int arg2)
         {
             string result = (arg1 + arg2).ToString();
-            arg2 += 20;
             Console.WriteLine("The number is: " + result);
         }
-        static void func2(int arg1, ref int arg2)
+        static void func2(int arg1, int arg2)
         {
             string result = (arg1 * arg2).ToString();
             Console.WriteLine("The number is: " + result);
@@ -30,12 +29,12 @@ namespace Composable2
 
             // call each delegate and then the chain
             Console.WriteLine("Calling the first delegate");
-            f1(a, ref b);
+            f1(a, b);
             Console.WriteLine("Calling the second delegate");
-            f2(a, ref b);
+            f2(a, b);
             // Call the composed delegate
             Console.WriteLine("\nCalling the chained delegates");
-            f1f2(a, ref b);
+            f1f2(a, b);
 
             Console.WriteLine("\nPress Enter Key to Continue...");
             Console.ReadLine();
